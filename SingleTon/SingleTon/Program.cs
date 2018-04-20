5e90faf87b809ec29a3a9313106dbba9046ac458
aLIEz2476 <sakuragun11@gmail.com>
2018-04-20 14:32:11 +0900

    1

--- SingleTon/SingleTon/Program.cs
+++ SingleTon/SingleTon/Program.cs
@@ -8,16 +8,20 @@ namespace SingleTon
 {
     class SingleObject
     {
+        
         static SingleObject m_cInstance;
+        // 정적할당 : 클래스 인스턴스를 만들지 않아도 접근 가능한 변수.
         SingleObject()
         {
             m_cInstance = null;
         }
-        public static SingleObject getInstance()
+        public static SingleObject getInstance() // 정적맴버
         {
+            // 클래스는 객체만으로 사용할 수 없다. 객체의 인스턴스를 만들지 않고 사용할 경우, NullReferenceException 발생
             if (m_cInstance == null)
                 m_cInstance = new SingleObject();
             return m_cInstance;
+            // if로 안 거를 경우, 중복정의로 인하여 포인터가 가리키는 인스턴스가 바뀌게 되고, 쓰던 인스턴스는 GC에 의해 썰린다.
         }
 
         public void ShowMessage()
